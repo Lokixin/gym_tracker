2a0de1b3021dab0746c248f6d631280043d10363
Lokixin <dimasavilamartinez@gmail.com>
2024-06-13 21:42:49 +0200

    Refactor add_exercise_metadata to use a single query (instead of 4)

--- src/gym_tracker/adapters/repositories.py
+++ src/gym_tracker/adapters/repositories.py
@@ -5,11 +5,8 @@ from psycopg import Connection
 
 from gym_tracker.adapters.admin_queries import (
     select_exercise_metadata_by_name,
-    select_muscle_group_by_name,
-    insert_exercise_metadata,
-    insert_secondary_muscle_groups,
 )
-from gym_tracker.domain.model import ExerciseMetadata
+from gym_tracker.domain.model import ExerciseMetadata, MuscleGroup
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -35,6 +32,29 @@ select_metadata_by_name = """
     GROUP BY ed.exercise_name, ed.primary_muscle_group;
 """
 
+find_muscle_groups_ids = """
+    WITH primary_insert AS (
+        INSERT 
+            INTO exercises_metadata (name, primary_muscle_group_id)
+        SELECT 
+            %s AS exercise_name,
+            id AS primary_muscle_id
+        FROM muscle_groups 
+            WHERE muscle_groups.muscle_group = %s
+        RETURNING id
+    ),
+    secondary_insert AS (
+        INSERT 
+            INTO metadata_secondary_muscle_group (metadata_id, muscle_group_id)
+        SELECT
+            primary_insert.id,
+            muscle_groups.id
+        FROM primary_insert, muscle_groups
+            WHERE muscle_groups.muscle_group = ANY(%s)
+    )
+    SELECT id FROM primary_insert
+"""
+
 
 class PostgresSQLRepo:
     def __init__(self, connection: Connection) -> None:
@@ -51,46 +71,18 @@ class PostgresSQLRepo:
             )
             return exercise_metadata
 
-    def add_exercise_metadata(self, exercise_metadata: ExerciseMetadata) -> None:
-        if self._check_if_metadata_already_exists(exercise_metadata.name):
-            logger.info(
-                f"Metadata not added: Already exists for {exercise_metadata.name}"
-            )
-            return
+    def add_exercise_metadata(self, exercise_metadata: ExerciseMetadata) -> tuple[int]:
         with self.conn.cursor() as cursor:
-            cursor.execute(
-                select_muscle_group_by_name,
-                [
-                    [
-                        exercise_metadata.primary_muscle_group,
-                        *exercise_metadata.secondary_muscle_groups,
-                    ]
-                ],
-            )
-            all_muscle_groups = cursor.fetchall()
-            # find primary
-            primary_muscle_group_id = -1
-            primary_muscle_group_pos = -1
-
-            for idx, muscle_group in enumerate(all_muscle_groups):
-                if muscle_group[1] == exercise_metadata.primary_muscle_group.value:
-                    primary_muscle_group_id = muscle_group[0]
-                    primary_muscle_group_pos = idx
-                    break
-
-            all_muscle_groups.pop(primary_muscle_group_pos)
-            secondary_muscle_groups_ids = [
-                muscle_group[0] for muscle_group in all_muscle_groups
+            primary_muscle_group = exercise_metadata.primary_muscle_group.value
+            secondary_muscle_group = [
+                str(mg.value) for mg in exercise_metadata.secondary_muscle_groups
             ]
             cursor.execute(
-                insert_exercise_metadata,
-                (exercise_metadata.name, primary_muscle_group_id),
+                find_muscle_groups_ids,
+                [exercise_metadata.name, primary_muscle_group, secondary_muscle_group],
             )
-            metadata_id = cursor.fetchone()[0]
-            cursor.executemany(
-                insert_secondary_muscle_groups,
-                [(metadata_id, _id) for _id in secondary_muscle_groups_ids],
-            )
+            result = cursor.fetchone()
+            return result
 
     def _check_if_metadata_already_exists(self, exercise_name: str) -> bool:
         with self.conn.cursor() as cursor:
@@ -104,5 +96,15 @@ if __name__ == "__main__":
     connection_string = "dbname=workouts host=localhost user=admin password=admin"
     with psycopg.connect(connection_string, autocommit=True) as conn:
         repo = PostgresSQLRepo(connection=conn)
-        exercise_metadata = repo.get_exercise_metadata_by_name("pull ups")
-        print(exercise_metadata)
+
+        exercise_metadata = ExerciseMetadata(
+            name="back squat",
+            primary_muscle_group=MuscleGroup.QUADRICEPS,
+            secondary_muscle_groups=[
+                MuscleGroup.GLUTEUS,
+                MuscleGroup.CALVES,
+                MuscleGroup.BACK,
+            ],
+        )
+        res = repo.add_exercise_metadata(exercise_metadata)
+        print(res)
