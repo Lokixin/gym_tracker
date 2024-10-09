24eb975fe90ad122468d8a6a14d079f4b8eec422
Lokixin <dimasavilamartinez@gmail.com>
2024-10-09 20:55:41 +0200

    Add endpoints to interact with workouts DB
    - Get full workotus by id/date
    - Add workout
    - Add exercise to workout
    - Add sets to exercise

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 ALL_PACKAGES := src tests
 
-.PHONY : reformat lint unit app
+.PHONY : reformat lint unit app kill
 
 lint:
 	poetry run ruff check $(ALL_PACKAGES) &
@@ -13,4 +13,7 @@ unit:
 	poetry run pytest .\tests\unit -s --log-level=INFO
 
 app:
-	poetry run uvicorn src.gym_tracker.entrypoints.fastapi_app:app --host localhost --port 5555 --reload
+	poetry run uvicorn src.gym_tracker.entrypoints.fastapi_app:app --host localhost --port 5555 --reload --log-level debug
+
+kill:
+	Stop-Process -Name "python" -force
--- src/gym_tracker/entrypoints/dtos.py
+++ src/gym_tracker/entrypoints/dtos.py
@@ -1,26 +1,47 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, Field
 
 from gym_tracker.domain.model import MuscleGroup
 
 
 class ExerciseSetDTO(BaseModel):
-    weight: float
-    repetitions: int
-    to_failure: bool | None = False
+    weight: Annotated[float, Field(..., examples=[100])]
+    repetitions: Annotated[int, Field(..., examples=[10])]
+    to_failure: Annotated[bool, Field(..., examples=[False])] | None = False
 
 
 class ExerciseMetadataDTO(BaseModel):
-    name: str
+    name: Annotated[str, Field(..., examples=["bench press"])]
     primary_muscle_group: MuscleGroup
-    secondary_muscle_groups: list[MuscleGroup]
+    secondary_muscle_groups: Annotated[
+        list[MuscleGroup], Field(..., examples=[["back", "biceps"]])
+    ]
 
 
 class ExerciseDTO(BaseModel):
-    exercise_metadata: ExerciseMetadataDTO
+    exercise_metadata: Annotated[
+        ExerciseMetadataDTO,
+        Field(
+            ...,
+            examples=[
+                {
+                    "name": "bench press",
+                    "primary_muscle_group": "peck",
+                    "secondary_muscle_groups": ["shoulders", "triceps"],
+                }
+            ],
+        ),
+    ]
     exercise_sets: list[ExerciseSetDTO]
 
 
 class WorkoutDTO(BaseModel):
-    date: str
-    duration: int
+    date: Annotated[str, Field(..., examples=["2024-06-16"])]
+    duration: Annotated[int, Field(..., examples=[120])]
     exercises: list[ExerciseDTO]
+
+
+class CreateWorkoutBody(BaseModel):
+    date: Annotated[str, Field(..., examples=["2024-06-16"])] | None = None
+    duration: Annotated[int, Field(..., examples=[90])] | None = 0
